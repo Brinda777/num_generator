432f394800462b8461eff0923d6b88629c24c6e7
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2024-05-22 11:41:30 +0545

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:num_generator/screen/game_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GameScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:num_generator/app/app.dart';
+import 'package:num_generator/screen/game_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
